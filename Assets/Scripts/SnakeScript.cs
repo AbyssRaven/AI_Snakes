37dcefadb585be78a8289d2b6bf7dc9dda8da18b
Bill Halim <bill9618halim@gmail.com>
2018-11-08 18:02:25 +0100

    New

---
+++ Assets/Scripts/SnakeScript.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.IMGUI.Controls;
+using UnityEditorInternal;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class SnakeScript : MonoBehaviour {
+
+	private SnakeScript _nextHead;
+	public static Action<String> collision;
+
+	public void SetNextHead(SnakeScript newHead) {
+		_nextHead = newHead;
+	}
+	
+	public SnakeScript GetNextHead() {
+		return _nextHead;
+	}
+
+	public void RemoveTail() 
+	{
+		Destroy(gameObject);
+	}
+
+	private void OnTriggerEnter(Collider other) {
+		if(collision != null) {
+			collision(other.transform.tag);
+		}
+
+		if(other.CompareTag("Food")) 
+		{
+			Destroy(other.gameObject);
+		}
+	}
+
+}
